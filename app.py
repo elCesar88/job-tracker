80da948be23bf787bd8399da8aba2f8911dcab39
elCesar88 <augustog88@live.ca>
2026-08-16 19:13:15 -0400

    add form to create new application

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import db
+
 
 app = Flask(__name__)
+db.init_db()
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    applications = db.get_all_applications()
+    return render_template("home.html", applications=applications)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        db.add_application(
+            company=request.form["company"],
+            role=request.form["role"],
+            status=request.form["status"],
+            date_applied=request.form["date_applied"],
+        )
+        return redirect(url_for("home"))
+    return render_template("add.html")
